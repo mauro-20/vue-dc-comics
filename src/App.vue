8aec4d17f37d53d6274b4ea142b25b386caf3fef
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-30 16:58:05 +0200

    add structure comics section

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <Header/>
     <main>
       <Hero/>
-      <Content/>
+      <Comics/>
+      <!-- <Content/> -->
       <Buy/>
     </main>
     <footer>
@@ -16,7 +17,8 @@
 <script>
 import Header from "./components/Header.vue";
 import Hero from "./components/Hero.vue";
-import Content from './components/Content.vue';
+import Comics from "./components/Comics.vue";
+// import Content from './components/Content.vue';
 import Buy from "./components/Buy.vue";
 import FooterTop from "./components/FooterTop.vue";
 import FooterBottom from "./components/FooterBottom.vue";
@@ -26,7 +28,8 @@ export default {
   components: {
     Header,
     Hero,
-    Content,
+    Comics,
+    // Content,
     Buy,
     FooterTop,
     FooterBottom
